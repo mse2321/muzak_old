f3fa5257f2c6a57f049ae7cbd1c2111b4764f953
mse2321 <mse233@gmail.com>
2015-08-16 22:54:17 -0400

    almost complete, just need to fix the play buttons

--- js/app.js
+++ js/app.js
@@ -7,48 +7,70 @@ $(function(){
 		$('.results').html('');
 		// get the value of the tags the user submitted
 		var artistName = $('#search').val();
-		getResults(artistName);
+		findArtist(artistName);
 	});
 
-	var getResults = function(artistName) {
-		
-		var q = {
-								name: artistName,
-								type: 'artist'};
-
-		var nameURL = encodeURI(q.name);
+	var findArtist = function(artistName) {
 		
 		// need to figure out how to get the ID from Spotify that matches the artist name the user types in
 		$.ajax({
-	 	 	url: 'https://api.spotify.com/v1/artists/' + '43ZHCT0cAZBISjO8DG9PnE' + '/top-tracks?country=US',
-	  		method: 'GET',
+	 	 	url: 'https://api.spotify.com/v1/search/',
+	 	 	data: {
+		  		q: artistName,
+		  		type: "artist"
+	  		}
 	  	})
-	  	.done(function (results) {
-	  		var audioURL = JSON.stringify(results.tracks[1].preview_url);
-	  		console.log(audioURL);
+	  	.done(function (results1) {
 
-
-	  		$("#results").css("display", "block");
-
-	  		$("#resultsData").each(function() {
-	  			$('#resultsData').html("<td>" + JSON.stringify(results.tracks[1].artists[0].name) + "</td>" + "<td>" + JSON.stringify(results.tracks[1].name) + "</td>"
-	  			+ "<td>" + JSON.stringify(results.tracks[1].album.name) + "</td>" +	"<td><i class='fa fa-play-circle'></i></td>");
-	  			$("source").attr("src", audioURL); //Need to be able to remove spaces from url when request is sent to retrieve file
-	  			$(".fa-play-circle").on("click", function() { 
-		  			$("#audioplayer").css("display", "block");
-		  			$("audio").load();
-				    $("audio").trigger('play');
-				})
-	  		})
+	  		console.log(JSON.stringify(results1.artists.items[0].name));
+	  		getSongs(results1);
 	  	})
 	  	.fail(function(jqXHR, error, errorThrown){
-			var errorElem = showError(error);
-			$('#ResultsData').append(errorElem);
+			alert("Something went wrong! - " + errorThrown);
 		});
 
 		/*
 		 */
 
 	};
+
+	function getSongs(results1) {
+				
+		// need to figure out how to get the ID from Spotify that matches the artist name the user types in
+		$.ajax({
+	 	 	url: 'https://api.spotify.com/v1/artists/' + results1.artists.items[0].id + '/top-tracks?country=US',
+	  		method: 'GET',
+	  	})
+	  	.done(function (results2) {
+	  		$("#results").css("display", "block");
+
+	  		for(i = 0; i < 11; i++) {
+	  			var trackURL = JSON.stringify(results2.tracks[i].preview_url).replace(/\"/g, "");
+
+		  		$('#resultsHeadings').after("<tr>" + "<td>" + JSON.stringify(results2.tracks[i].name).replace(/\"/g, "") + "</td>"
+		  		+ "<td>" + JSON.stringify(results2.tracks[i].album.name).replace(/\"/g, "") + "</td>" +	"<td><a href='" + trackURL + "' target='_blank'><i class='fa fa-play-circle'></i></a></td></tr>");
+		  		//playSongs(trackURL);
+	  		}
+	  		
+	  	})
+	  	.fail(function(jqXHR, error, errorThrown){
+			alert("Something went wrong! - " + errorThrown);
+		});
+
+	};
+
+	// need to figure out how to play the individual files within Muzak
+	/* function playSongs(trackURL) {
+		$(".fa-play-circle").each(function(){
+		  	$(this).on("click", function() {
+				console.log(trackURL);
+		  		$("source").attr("src", trackURL); //Need to be able to remove spaces from url when request is sent to retrieve file
+				$("#audioplayer").css("display", "block");
+				$("audio").load();
+				$("audio").trigger('play');
+
+			});
+		});
+	} */
 
 });
