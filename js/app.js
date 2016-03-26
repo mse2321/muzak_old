f8fd0a506c87ae8795c946c7b7a3c3c6007a230f
mse2321 <mse233@gmail.com>
2016-03-25 20:36:51 -0400

    almost there but still passing an error

--- js/app.js
+++ js/app.js
@@ -12,10 +12,20 @@ $(function(){
 
 		if(submissions === 0) {
 			findArtist(artistName);
+			hideInstructions();
 		} else {
 			location.reload();
 		}
 		++submissions;
+
+	}
+
+	function hideInstructions() {
+		$("header > p").hide();
+
+		if ( $(window).width() < 1000) {
+			$("#search_form").css("margin", "0 auto 2em auto");
+		}
 	}
 
 	var findArtist = function(artistName) {
@@ -28,37 +38,44 @@ $(function(){
 		  		type: "artist"
 	  		}
 	  	})
-	  	.done(function (results1) {
-	  		getSongs(results1);
-	  		console.log('results1' + results1);
+	  	.done(function (results) {
+	  		getSongs(results);
 	  	})
 	  	.fail(function(jqXHR, error, errorThrown){
 			alert("Something went wrong! - " + errorThrown);
 		});
 
-		/*
-		 */
-
 	};
 
-	function getSongs(results1) {
+	function getSongs(results) {
 				
 		// need to figure out how to get the ID from Spotify that matches the artist name the user types in
 		$.ajax({
-	 	 	url: 'https://api.spotify.com/v1/artists/' + results1.artists.items[0].id + '/top-tracks?country=US',
+	 	 	url: 'https://api.spotify.com/v1/artists/' + results.artists.items[0].id + '/top-tracks?country=US',
 	  		method: 'GET',
 	  	})
-	  	.done(function (results2) {
+	  	.done(function (results) {
 	  		$("#results").css("display", "block");
 
-	  		console.log('results2' + results2);
+	  		for(i = 0; i < 10; i++) {
+	  			var trackURL = results.tracks[i].preview_url;
+	  			//var trackURL = JSON.stringify(results.tracks[i].preview_url).replace(/\"/g, "");
+	  			if($(window).width() < 1000) {
+	  				var albumArt = results.tracks[i].album.images[0].url;
+	  			} else {
+	  				var albumArt = results.tracks[i].album.images[1].url;
+	  			}
 
-	  		for(i = 0; i < 11; i++) {
-	  			var trackURL = JSON.stringify(results2.tracks[i].preview_url).replace(/\"/g, "");
+	  			var songName = results.tracks[i].name;
+	  			var albumName = results.tracks[i].album.name;
 
-		  		$('#resultsHeadings').after("<tr class='tracks'>" + "<td>" + JSON.stringify(results2.tracks[i].name).replace(/\"/g, "") + "</td>"
-		  		+ "<td>" + JSON.stringify(results2.tracks[i].album.name).replace(/\"/g, "") + "</td>" +	"<td class='hidden'>" + trackURL + "</td>" + "<td><i class='fa fa-play-circle'></i></td></tr>");
-		  		playSongs(trackURL);
+				$('#resultsHeadings').after("<tr class='tracks'>" + "<td>" + songName + "</td>" + "<td class='hidden'>" + trackURL + "</td>" + "<td class='play_button'><i class='fa fa-play-circle'></i></td></tr>");  
+
+				playSongs(trackURL, albumArt, songName, albumName);
+				showPlayer();
+		  		hidePlayer();	
+		  		pauseSongs();
+		  		replaySongs();
 	  		}
 	  		
 	  	})
@@ -68,18 +85,73 @@ $(function(){
 
 	};
 
-	// need to figure out how to play the individual files within Muzak
-	function playSongs(trackURL) {
+	function showPlayer() {
 
-		$(".fa-play-circle").each(function(){
+		$(".fa-play-circle").click(function(){
+			$("#audioPlayer").show("slide", { direction: "right" });
+
+			if ( $(window).width() < 1000) {
+				$("#search_form, #results, header > p").hide("slide", { direction: "left" });
+			}
+			
+			$("header").css("padding", "20px 20px 0px 20px");
+		});
+
+	}
+
+	function hidePlayer() {
+
+		$("#mobile_back > p").click(function(){
+			$("#audioPlayer").hide("slide", { direction: "right" });
+			
+			if ( $(window).width() < 1000) {
+				$("#search_form, #results").show("slide", { direction: "left" });
+			}
+
+			$("header").css("padding", "20px");
+		});
+
+	}
+
+	function playSongs(trackURL, albumArt, songName, albumName) {
+
+			  			console.log(trackURL);
+
+		$("tr").each(function(){
 		  	$(this).on("click", function() {			
-		  		$("source").attr("src", trackURL); //Need to be able to remove spaces from url when request is sent to retrieve file
+		  		$("source").attr("src", trackURL);
+		  		$(".album_art").attr('src', albumArt);
+		  		$("#song_name_display").html("<p>" + songName + "</p><p class='album'>" + albumName + "</p>");
 				$("audio").load();
+				//console.log('Results: ' + ($(this).html()));
 				$("audio").trigger('play');
-				$("#audioplayer").css("display", "block");
-				$(this)[0].cells[1].innerText;
+				$(".fa-pause").removeClass("active");
+				$(".fa-play").addClass("active");
+				$(this).cells[1].text();
+
 			});
 		});
+
+	}
+
+	function pauseSongs() {
+
+		$(".fa-pause").on("click", function() {
+			$(".fa-play").removeClass("active");
+			$(".fa-pause").addClass("active");	
+			$("audio").trigger('pause');
+		});
+
+	}
+
+	function replaySongs() {
+
+		$(".fa-play").on("click", function() {
+			$(".fa-pause").removeClass("active");
+			$(".fa-play").addClass("active");
+			$("audio").trigger('play');
+		});
+
 	}
 
 
