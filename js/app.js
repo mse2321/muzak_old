425583812257bc0dd8722623167f5fc5116cb9fe
mse2321 <mse233@gmail.com>
2016-03-28 17:24:02 -0400

    angular conversion complete. Also set up audio player directive. Pause button no longer working

--- js/app.js
+++ js/app.js
@@ -1,6 +1,32 @@
 var demo = angular.module('demo', []);
 
-	demo.controller('ctrl', function($scope){
+
+	// gets artist
+	demo.factory('artist', function($http){
+	            return function(artistName){
+	              return $http ({ 
+	                method: 'GET', 
+	                url: 'https://api.spotify.com/v1/search/',
+	                params: {
+			  			q: artistName,
+			  			type: "artist"
+		  			}
+	              })
+	            };
+	}); // end of artist
+
+	// gets songs
+	demo.factory('songs', function($http){
+	            return function(artist_id){
+	              return $http ({ 
+	                method: 'GET', 
+	                url: 'https://api.spotify.com/v1/artists/' + artist_id + '/top-tracks?country=US'
+	                //params: {callback: 'JSON_CALLBACK'}
+	              })
+	            };
+	}); // end of songs
+
+	demo.controller('ctrl', function($scope, artist, songs){
 		$scope.submissions = 0;
 
 		$scope.sendArtistData = function() {
@@ -8,142 +34,126 @@ var demo = angular.module('demo', []);
 
 			if($scope.submissions === 0) {
 				$scope.findArtist($scope.artistName);
-				$scope.hideInstructions();
 			} else {
 				location.reload();
 			}
 			++$scope.submissions;
 		};
 
-		$scope.hideInstructions = function() {
-			$("header > p").hide();
-
-			if ( $(window).width() < 1000) {
-				$("#search_form").css("margin", "0 auto 2em auto");
-			}
-		};
-
 		$scope.findArtist =  function(artistName) {
-		
-			$.ajax({
-		 	 	url: 'https://api.spotify.com/v1/search/',
-		 	 	data: {
-			  		q: artistName,
-			  		type: "artist"
-		  		}
-		  	})
-		  	.done(function (results) {
+
+			//$scope.artistName = artistName.;
+			console.log(artistName);
+
+		  	artist(artistName).success(function (results) {
+		  		console.log(results);
 		  		$scope.getSongs(results);
 		  	})
-		  	.fail(function(jqXHR, error, errorThrown){
-				alert("Something went wrong! - " + errorThrown);
+		  	.error(function(results){
+				console.log("Error!");
+				console.log(results);
 			});
+
+			$("#results").show();
 
 		};
 
 		$scope.getSongs = function(results) {
-				
-			$.ajax({
-		 	 	url: 'https://api.spotify.com/v1/artists/' + results.artists.items[0].id + '/top-tracks?country=US',
-		  		method: 'GET',
-		  	})
-		  	.done(function (results) {
 
-		  		$scope.data = results;
+			$scope.artist_id = results.artists.items[0].id;
+			console.log($scope.artist_id);
+				
+		  	songs($scope.artist_id).success(function (results2) {
+
+		  		console.log(results2);
+		  	
 		  		$scope.tracks_list = [];
 
 		  		for(i = 0; i < 10; i++) {
 
-		  			$scope.songName = $scope.data.tracks[i].name;
-		  			$scope.albumArt = $scope.data.tracks[i].album.images[1].url;
-		  			$scope.track_url = $scope.data.tracks[i].preview_url;
-		  			$scope.albumName = $scope.data.tracks[i].album.name;
+		  			$scope.songName = results2.tracks[i].name;
+		  			$scope.albumArt = results2.tracks[i].album.images[1].url;
+		  			$scope.track_url = results2.tracks[i].preview_url;
+		  			$scope.albumName = results2.tracks[i].album.name;
+		  			$scope.song_index = i;
 		  			
 		  			$scope.track_item = {
 						name: $scope.songName,
 						albumName: $scope.albumName,
 						track_url: $scope.track_url,
-						albumArt: $scope.albumArt
+						albumArt: $scope.albumArt,
+						id: $scope.song_index
 					}; 
 
 					$scope.tracks_list.push($scope.track_item);
 
 		  		};
-				/* $scope.playSongs($scope.track_url, $scope.albumArt, $scope.songName, $scope.albumName);
+		  		//console.log($scope.tracks_list);
+				$scope.playSongs($scope.tracks_list);
 				$scope.showPlayer();
 			  	$scope.hidePlayer();	
-			  	$scope.pauseSongs();
-			  	$scope.replaySongs(); */
 		  		
 		  	})
-		  	.fail(function(jqXHR, error, errorThrown){
-				alert("Please type in an artist's name.");
+		  	.error(function(results){
+				console.log("Error!" + results);
 			});
-			console.log($scope.tracks_list);
 
 		};
 
 	$scope.showPlayer = function() {
 
-		$(".fa-play-circle").click(function(){
-			$("#audioPlayer").show("slide", { direction: "right" });
-
-			if ( $(window).width() < 1000) {
+		if ( $(window).width() < 1000) {
+				$("#audioPlayer").show("slide", { direction: "right" });
 				$("#search_form, #results, header > p").hide("slide", { direction: "left" });
 			}
 			
 			$("header").css("padding", "20px 20px 0px 20px");
-		});
-
 	};
 
 	$scope.hidePlayer = function() {
 
-		$("#mobile_back > p").click(function(){
-			$("#audioPlayer").hide("slide", { direction: "right" });
-			
-			if ( $(window).width() < 1000) {
+		if ( $(window).width() < 1000) {
+				$("#audioPlayer").hide("slide", { direction: "right" });
 				$("#search_form, #results").show("slide", { direction: "left" });
 			}
 
 			$("header").css("padding", "20px");
-		});
 
 	};
 
-	$scope.playSongs = function(track_url, albumArt, songName, albumName) {
+	$scope.playSongs = function(tracks_list) {
 
-		console.log(track_url);
+		$scope.itemNumber = $(this)[0].item.id;
 
-		$("source").attr("src", track_url);
-		$(".album_art").attr('src', albumArt);
-		$("#song_name_display").html("<p>" + songName + "</p><p class='album'>" + albumName + "</p>");
+		$("source").attr("src", $scope.tracks_list[$scope.itemNumber].track_url);
+		$(".album_art").attr('src', $scope.tracks_list[$scope.itemNumber].albumArt);
+		$("#song_name_display").html("<p>" + $scope.tracks_list[$scope.itemNumber].name + "</p><p class='album'>" + $scope.tracks_list[$scope.itemNumber].albumName + "</p>");
 		$("audio").load();
-		$("audio").trigger('play');
+		$("#audioPlayer").show();
+		$("#music").trigger('play');
 		$(".fa-pause").removeClass("active");
 		$(".fa-play").addClass("active");
-		//$(this).track.track_url;
 
 	};
 
 	$scope.pauseSongs = function() {
-
-		$(".fa-pause").on("click", function() {
-			$(".fa-play").removeClass("active");
-			$(".fa-pause").addClass("active");	
-			$("audio").trigger('pause');
-		});
-
+		$(".fa-play").removeClass("active");
+		$(".fa-pause").addClass("active");	
+		$("#music").trigger("pause");
 	};
 
 	$scope.replaySongs = function() {
-
-		$(".fa-play").on("click", function() {
-			$(".fa-pause").removeClass("active");
-			$(".fa-play").addClass("active");
-			$("audio").trigger('play');
-		});
-
+		$(".fa-pause").removeClass("active");
+		$(".fa-play").addClass("active");
+		$("#music").trigger("play");
 	};
 
+})
+demo.directive('audioPlayer', function() {
+	return {
+		templateUrl: 'audioPlayer.html',
+		restrict: 'E',
+		scope: true,
+		transclude: true
+	}
 });
